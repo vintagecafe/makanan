765f1e6339a7d049954733beb24e1fbbb33efb1a
Vinta <Vinta@Vinta-PC>
2015-04-17 10:08:48 +0700

    buat project cafe unik

---
+++ src/vintagecafe/Vintagecafe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vintagecafe;
+
+/**
+ *
+ * @author Vinta
+ */
+public class Vintagecafe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
